60260f2a60215cb00244ab52947af1327c8da214
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-04-27 16:36:43 +0900

    React state 관리와 업데이트
    - 상태 관리와 상호작용 기능 구현 useState와 useImmer 활용하여 카운터 및 폼 컴포넌트 추가

--- react-07-state-interact/src/App.tsx
+++ react-07-state-interact/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Counter from "./Couter";
 import ProcessQueue from "./statequeue/ProcessQueue";
 
 function App() {
   return (
     <>
       <div></div>
+      <Counter />
       <ProcessQueue />
     </>
   );
